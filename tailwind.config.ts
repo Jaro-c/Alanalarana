9b68f4739b669c292826c138621555cdc251b8e8
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-10-03 16:49:14 -0500

    🎨 style: add color palette to Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,33 @@ const config: Config = {
 	theme: {
 		extend: {
 			colors: {
-				primary: "",
-				secondary: "",
+				primary: {
+					50: "#eefff6",
+					100: "#d7ffeb",
+					200: "#b2ffd9",
+					300: "#76ffbd",
+					400: "#33f599",
+					500: "#09de79", // Main Color
+					600: "#00b560",
+					700: "#049150",
+					800: "#0a7142",
+					900: "#0a5d38",
+					950: "#00341d",
+				},
+				secondary: {
+					50: "#f4f5f9",
+					100: "#eaedf5",
+					200: "#dde0ee", // Main Color
+					300: "#c1c6e0",
+					400: "#a8abd1",
+					500: "#9192c3",
+					600: "#7e7ab1",
+					700: "#6d689a",
+					800: "#59567d",
+					900: "#4b4966",
+					950: "#2c2b3b",
+				},
+				red: "#FF0000",
 			},
 			screens: {
 				"3xl": "1920px",
