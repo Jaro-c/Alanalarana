6c0baac67a61a5d93556663d304e1bc9efdaff24
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-10-04 16:38:01 -0500

    🚀 feat: add profile and socials section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 import styles from "./styles.module.css";
+
+const Profile = dynamic(() => import("@/components/home/profile/main"));
+const SocialMedia = dynamic(() => import("@/components/home/socials/main"));
 
 const Developers = dynamic(() => import("@/components/home/developers"));
 
 export default function Home() {
 	return (
 		<div className={`${styles["background-section"]} h-full`}>
-			<div className="flex flex-col">
-				<div>
-					<div></div>
+			<div className="flex h-full flex-col">
+				<div className="container flex h-full flex-col py-8 lg:flex-row">
+					{/* Alana: Profile */}
+					<div className="w-full lg:w-1/2">
+						<Suspense>
+							<Profile />
+						</Suspense>
+					</div>
+
+					{/* Alana: Socials */}
+					<div className="w-full lg:w-1/2">
+						<Suspense>
+							<SocialMedia />
+						</Suspense>
+					</div>
 				</div>
 
 				{/* Developers */}
