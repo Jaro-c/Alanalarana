37b972c16d4b4526979e6132c9332c338bd39eda
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-10-03 20:34:26 -0500

    🚀 feat: add Developer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import dynamic from "next/dynamic";
+
 import styles from "./styles.module.css";
+
+const Developers = dynamic(() => import("@/components/home/developers"));
 
 export default function Home() {
 	return (
 		<div className={`${styles["background-section"]} h-full`}>
-			<div className="text-white">test</div>
+			<div className="flex flex-col">
+				<div>
+					<div></div>
+				</div>
+
+				{/* Developers */}
+				<Developers />
+			</div>
 		</div>
 	);
 }
---
+++ src/components/home/developers.tsx
@@ -0,0 +1,7 @@
+export default function name() {
+	return (
+		<div className="container flex justify-center">
+			<p className="text-secondary-200">Desarrollado por Jar0_c y Busta</p>
+		</div>
+	);
+}
