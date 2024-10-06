26469d05e58903c6710d62ec7cb3b4ed8a3d99fc
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-10-06 16:51:03 -0500

    📝 docs: add developer information

---
+++ src/components/developers.tsx
@@ -0,0 +1,17 @@
+export default function Developers() {
+	return (
+		<div className="container flex justify-center">
+			<p className="text-secondary-200/40">
+				Desarrollado por{" "}
+				<a className="transition-colors duration-300 ease-in-out hover:text-primary-500" href="https://github.com/Jaro-c" target="_blank" rel="noopener">
+					Jar0_c
+				</a>{" "}
+				y diseñado por{" "}
+				<a className="transition-colors duration-300 ease-in-out hover:text-primary-500" href="https://github.com/BustaGraphic" target="_blank" rel="noopener">
+					Busta
+				</a>
+				.
+			</p>
+		</div>
+	);
+}
--- src/components/home/developers.tsx
+++
@@ -1,7 +0,0 @@
-export default function Developers() {
-	return (
-		<div className="container flex justify-center">
-			<p className="text-secondary-200">Desarrollado por Jar0_c y Busta</p>
-		</div>
-	);
-}
