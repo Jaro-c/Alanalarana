99495b2b2bd117d035ac54951d87cf7c4800e362
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-10-06 17:04:38 -0500

    📝 docs: update developer name

--- src/components/developers.tsx
+++ src/components/developers.tsx
@@ -4,7 +4,7 @@ export default function Developers() {
 			<p className="text-secondary-200/40">
 				Desarrollado por{" "}
 				<a className="transition-colors duration-300 ease-in-out hover:text-primary-500" href="https://github.com/Jaro-c" target="_blank" rel="noopener">
-					Jar0_c
+					Alejandro
 				</a>{" "}
 				y diseñado por{" "}
 				<a className="transition-colors duration-300 ease-in-out hover:text-primary-500" href="https://github.com/BustaGraphic" target="_blank" rel="noopener">
