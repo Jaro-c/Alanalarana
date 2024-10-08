ff546768f812684521eb4c64ff9cad2e1c203df2
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-10-08 11:45:01 -0500

    🗺️ chore(sitemap): add raniza page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,20 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+	const MainUrl = process.env.MD_URL as string;
+
 	return [
 		{
-			url: process.env.MD_URL as string,
+			url: MainUrl,
 			lastModified: new Date(),
 			changeFrequency: "daily",
 			priority: 1,
+		},
+		{
+			url: MainUrl + "/raniza",
+			lastModified: new Date(),
+			changeFrequency: "daily",
+			priority: 0.9,
 		},
 	];
 }
