a511285186fe0c5344699917cd57c441d9bcbd7f
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-10-10 12:12:54 -0500

    💄 style: add margin to hover action for socials

--- src/components/home/socials/main.tsx
+++ src/components/home/socials/main.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 const YouTube = dynamic(() => import("@/components/home/socials/youtube/main"));
 
 export default function Social_Media() {
-	const social_width = "w-full md:max-xl:w-[90%] xl:w-[80%] 2xl:w-[70%] button-transition md:hover:scale-105";
+	const social_width = "w-full md:max-xl:w-[90%] xl:w-[80%] 2xl:w-[70%] button-transition md:hover:scale-105 md:hover:my-2";
 
 	return (
 		<nav className="w-full lg:h-[90vh] xl:h-[80vh] 2xl:h-[90vh]">
