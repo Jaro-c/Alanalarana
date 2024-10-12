3dd4a09676e8706207d03e17bf3371cea120c2b2
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-10-12 12:28:35 -0500

    💄 style(button): change padding of Twitch button

--- src/components/home/socials/twitch/stream.tsx
+++ src/components/home/socials/twitch/stream.tsx
@@ -11,7 +11,7 @@ export default async function Stream() {
 					<div className="absolute z-0 h-4/5 w-[70%] animate-ping rounded-full bg-red"></div>
 				</div>
 			) : (
-				<div className="rounded-full border border-gray-dark/10 bg-gray-darker px-2 py-0.5">
+				<div className="rounded-full border border-gray-dark/10 bg-gray-darker px-3 py-0.5">
 					<span className="select-none text-sm font-medium text-secondary-200 lg:text-base">Offline</span>
 				</div>
 			)}
