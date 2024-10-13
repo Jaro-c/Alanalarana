e64f8a1bfb8523935ea15e754c1ef1564402797d
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-10-13 13:33:03 -0500

    🔧 chore: update Instagram cache revalidation from 6 hours to 8

--- src/api/instagram/AccountInfo.ts
+++ src/api/instagram/AccountInfo.ts
@@ -11,7 +11,7 @@ export default async function AccountInfo(username: string) {
 		const response = await fetch("https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url=" + username, {
 			method: "GET",
 			headers: { "x-rapidapi-key": ClientKey, "x-rapidapi-host": "instagram-scraper-api2.p.rapidapi.com" },
-			next: { revalidate: 6 * 60 * 60 }, // 6 hours
+			next: { revalidate: 8 * 60 * 60 }, // 8 hours
 		});
 
 		const data = await response.json();
