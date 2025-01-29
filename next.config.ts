d39449e7bece3cdf948895356ef4d88b6f221c83
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2025-01-29 11:09:14 -0800

    🔧 chore(config): upgrade Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	compress: false, // Nginx compression
 	poweredByHeader: false,
+	experimental: {
+		optimizePackageImports: ["millify"],
+	},
 	images: {
 		remotePatterns: [
 			{
 				protocol: "https",
 				hostname: "i.ytimg.com",
-				port: "",
 				pathname: "/vi/**",
 			},
 		],
-	},
-	experimental: {
-		nextScriptWorkers: true,
 	},
 };
 
